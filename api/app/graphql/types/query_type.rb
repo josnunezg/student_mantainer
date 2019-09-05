a43be807e0b50be364f6c7540b62c8b4971b9935
Jose Nuñez <josnunezg@gmail.com>
2019-09-05 02:15:24 -0400

    add frontend and finish api

--- api/app/graphql/types/query_type.rb
+++ api/app/graphql/types/query_type.rb
@@ -4,10 +4,18 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :student, Types::StudentType, null: true, description: "One Student" do
+      argument :id, ID, required: true
+    end
+
+    def student(id:)
+      Student.find_by_id(id)
+    end
+
+    field :students, [Types::StudentType], null: false, description: "All students"
+
+    def students
+      Student.all
     end
   end
 end
--- api/app/models/student.rb
+++ api/app/models/student.rb
@@ -1,2 +1,9 @@
 class Student < ApplicationRecord
+
+  validates_presence_of :rut
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
